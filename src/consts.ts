c7b85cfe244033131c9c6faa47d06c03504803e0
p4rr4 <jesuspg162004@gmail.com>
2024-12-11 13:09:56 +0100

    Migrate old blog

--- src/consts.ts
+++ src/consts.ts
@@ -13,25 +13,23 @@ export type Link = {
 }
 
 export const SITE: Site = {
-  TITLE: 'astro-erudite',
+  TITLE: 'p4rr4',
   DESCRIPTION:
-    'astro-erudite is a opinionated, no-frills blogging template—built with Astro, Tailwind, and shadcn/ui.',
-  EMAIL: 'jason@enscribe.dev',
+    'Cybersecurity Blog',
   NUM_POSTS_ON_HOMEPAGE: 2,
   POSTS_PER_PAGE: 3,
-  SITEURL: 'https://astro-erudite.vercel.app',
+  SITEURL: 'https://p4rr4.github.io',
 }
 
 export const NAV_LINKS: Link[] = [
   { href: '/blog', label: 'blog' },
-  { href: '/authors', label: 'authors' },
   { href: '/about', label: 'about' },
   { href: '/tags', label: 'tags' },
 ]
 
 export const SOCIAL_LINKS: Link[] = [
-  { href: 'https://github.com/jktrn', label: 'GitHub' },
-  { href: 'https://twitter.com/enscry', label: 'Twitter' },
-  { href: 'jason@enscribe.dev', label: 'Email' },
+  { href: 'https://github.com/p4rr4', label: 'GitHub' },
+  { href: 'https://twitter.com/_p4rr4', label: 'Twitter' },
+  { href: 'https://linkedin.com/in/p4rr4', label: 'LinkedIn' },
   { href: '/rss.xml', label: 'RSS' },
 ]
---
+++ src/content/authors/p4rr4.md
@@ -0,0 +1,10 @@
+---
+name: 'p4rr4'
+pronouns: 'he/him'
+avatar: 'http://localhost:1234/static/avatar.jpg'
+bio: 'rambling around web stuff'
+linkedin: 'https://linkedin.com/in/p4rr4'
+website: 'https://p4rr4.github.io'
+twitter: 'https://twitter.com/_p4rr4'
+github: 'https://github.com/p4rr4'
+---
